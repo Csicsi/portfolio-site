0460c32571f811e294f27e04ef4ffd9c418c0045
csicsak.daniel <csicsak.daniel@gmail.com>
2025-06-25 06:39:33 +0200

    removed tests for now

--- src/__tests__/App.test.jsx
+++
@@ -1,8 +0,0 @@
-/* global test, expect */
-import { render, screen } from '@testing-library/react';
-import App from '../App';
-
-test('renders welcome message', () => {
-  render(<App />);
-  expect(screen.getByText(/Welcome to/i)).toBeInTheDocument();
-});
